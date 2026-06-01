126aa6b5050df945a8da528f9930641beb8e0403
HYOJIN <hjhwang08@naver.com>
2026-06-01 15:31:40 +0900

    feat: initial commit

---
+++ src/main/java/plantly/plantly/PlantlyApplication.java
@@ -0,0 +1,13 @@
+package plantly.plantly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/plantly/plantly/PlantlyApplicationTests.java
@@ -0,0 +1,13 @@
+package plantly.plantly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
